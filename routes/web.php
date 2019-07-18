33d701ae49dbdf19eacf5a6281515d934632d16d
Risky Muaji <muaji.risky@gmail.com>
2019-07-18 09:35:22 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
+
 Route::get('user/{id}', 'UserController@showProfile');
 Route::get('passport/{id}', 'UserController@showPassport');
 Route::get('lesson/{id}', 'UserController@showLesson');
-=======
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
->>>>>>> 260a838feff1ff2b276cc4045ce398aec694a1a3
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
